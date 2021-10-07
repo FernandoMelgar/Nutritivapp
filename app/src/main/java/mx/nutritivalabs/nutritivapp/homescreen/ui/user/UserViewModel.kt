9b95479e673de4d663a869974a098c52695ff4d3
artur3456 <a01376086@itesm.mx>
2021-10-07 02:18:41 -0500

    Implementation of the home screen, user screen, configuration screen and their navigations.

---
+++ app/src/main/java/mx/nutritivalabs/nutritivapp/homescreen/ui/user/UserViewModel.kt
@@ -0,0 +1,9 @@
+package mx.nutritivalabs.nutritivapp.homescreen.ui.user
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class UserViewModel : ViewModel() {
+
+}
