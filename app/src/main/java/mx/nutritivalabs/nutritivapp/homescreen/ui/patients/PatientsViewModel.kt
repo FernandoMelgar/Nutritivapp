08c5602b38177d04c2b67b2562d9bbabce138e34
artur3456 <a01376086@itesm.mx>
2021-10-07 02:18:41 -0500

    Implementation of the calendar screen (home screen)

---
+++ app/src/main/java/mx/nutritivalabs/nutritivapp/homescreen/ui/patients/PatientsViewModel.kt
@@ -0,0 +1,13 @@
+package mx.nutritivalabs.nutritivapp.homescreen.ui.patients
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class PatientsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
