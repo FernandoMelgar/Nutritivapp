a816e14456d6ad1841138f0f7cfb672bffdbdbf9
Fernando Melgar <fernandom.melgar@gmail.com>
2021-10-11 00:05:01 -0500

    Jetpack Compose Material theme

---
+++ app/src/main/java/mx/nutritivalabs/nutritivapp/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package mx.nutritivalabs.nutritivapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val black = Color(0xFF0A0908)
+val white = Color(0xFFFEFEFA)
+
+val green700 = Color(0xFF46D5B3)
+val green500 = Color(0xFF7EE2CA)
+val green300 = Color(0xFFA3EAD9)
+
+val blue700 = Color(0xFF172A3A)
+val blue500 = Color(0xFF455561)
+val blue300 = Color(0xFF747F89)
---
+++ app/src/main/java/mx/nutritivalabs/nutritivapp/ui/theme/Theme.kt
@@ -0,0 +1,44 @@
+package mx.nutritivalabs.nutritivapp.ui.theme
+
+import android.content.res.Configuration
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+/*
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+*/
+
+private val LightColorPalette = lightColors(
+    primary = green500,
+    primaryVariant = green700,
+    secondary = blue500,
+    secondaryVariant = blue700,
+    background = white,
+    surface = white,
+    onPrimary = black,
+    onSecondary = white,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+)
+
+@Composable
+fun NutritivappTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
+    MaterialTheme(
+        colors = LightColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
