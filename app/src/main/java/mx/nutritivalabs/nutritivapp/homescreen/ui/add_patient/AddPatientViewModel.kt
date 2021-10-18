f5883d3bb390208eb2bb2410cdef29d9f7e827d6
Ruben Villalpando Bremont <a01376331@itesm.mx>
2021-10-17 19:18:46 -0500

    Creé el adaptador de los renglones de los pacientes
    Falta:
    - enlazar el FAB para que salga el fragmento
    - Poner el diseño de los renglones y el fragmento de add_patient_fragment.xml
    - poner los datos del paciente en los renglones

---
+++ app/src/main/java/mx/nutritivalabs/nutritivapp/homescreen/ui/add_patient/AddPatientViewModel.kt
@@ -0,0 +1,7 @@
+package mx.nutritivalabs.nutritivapp.homescreen.ui.add_patient
+
+import androidx.lifecycle.ViewModel
+
+class AddPatientViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/mx/nutritivalabs/nutritivapp/homescreen/ui/patients/RowListener.kt
@@ -0,0 +1,7 @@
+package mx.nutritivalabs.nutritivapp.homescreen.ui.patients
+
+interface RowListener {
+
+    fun rowClick(position: Int)
+
+}
