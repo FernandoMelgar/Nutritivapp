8626680e85908635ba62f4958f438d432d761ffb
Alex Serrano <a01376364@itesm.mx>
2021-10-22 17:57:11 -0500

    Creé el botón de sign in, la pantalla inicial no enseña el botón si ya tienes cuenta y el botón de sign out ya tiene funcionalidad

--- app/src/main/java/mx/nutritivalabs/nutritivapp/MainActivity.kt
+++ app/src/main/java/mx/nutritivalabs/nutritivapp/MainActivity.kt
@@ -3,10 +3,21 @@ package mx.nutritivalabs.nutritivapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 import mx.nutritivalabs.nutritivapp.databinding.ActivityMainBinding
 import mx.nutritivalabs.nutritivapp.homescreen.HomeScreen
 
 class MainActivity : AppCompatActivity() {
+
+    // Vals para Signin
+    private val CODIGO_SIGNIN: Int = 500
+    private val mAuth = FirebaseAuth.getInstance()
 
     private lateinit var binding: ActivityMainBinding
 
@@ -19,10 +30,64 @@ class MainActivity : AppCompatActivity() {
         configurarEventos()
     }
 
+    override fun onStart() {
+        super.onStart()
+        val usuario = mAuth.currentUser
+        if (usuario != null){                               // El usuario está dentro, se le da la bienvenida
+            binding.btnEntrar.visibility = View.GONE        // y accede al HomeScreen
+            println("INICIA: ${usuario?.displayName}")
+            Handler(Looper.getMainLooper()).postDelayed(
+                {
+                    val intentHomeScreen = Intent(this, HomeScreen::class.java)
+                    startActivity(intentHomeScreen)
+                },
+                1000
+            )
+        } else {
+            println("Hacer SignIn")
+        }
+    }
+
+    private fun autenticar() {
+        val providers = arrayListOf(AuthUI.IdpConfig.GoogleBuilder().build())
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            CODIGO_SIGNIN
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CODIGO_SIGNIN){
+            when (resultCode) {
+                RESULT_OK -> {
+                    val usuario = FirebaseAuth.getInstance().currentUser
+                    println("Bienvenido: ${usuario?.displayName}")
+                    println("Correo: ${usuario?.email}")
+                    println("UID: ${usuario?.uid}")
+                    val intentHomeScreen = Intent(this, HomeScreen::class.java)
+                    startActivity(intentHomeScreen)
+                }
+                RESULT_CANCELED -> {
+                    println("Cancelado...")
+                    val response = IdpResponse.fromResultIntent(data)
+                    println("Error: ${response?.error?.localizedMessage}")
+                }
+                else -> {
+                    val response = IdpResponse.fromResultIntent(data)
+                    println("Error: ${response?.error?.localizedMessage}")
+                }
+            }
+        }
+    }
+
     private fun configurarEventos() {
         binding.btnEntrar.setOnClickListener {
-            val intentHomeScreen = Intent(this, HomeScreen::class.java)
-            startActivity(intentHomeScreen)
+            autenticar()
         }
     }
 }
--- app/src/main/java/mx/nutritivalabs/nutritivapp/homescreen/ui/user/UserFragment.kt
+++ app/src/main/java/mx/nutritivalabs/nutritivapp/homescreen/ui/user/UserFragment.kt
@@ -1,16 +1,21 @@
 package mx.nutritivalabs.nutritivapp.homescreen.ui.user
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.auth.FirebaseAuth
+import mx.nutritivalabs.nutritivapp.MainActivity
 import mx.nutritivalabs.nutritivapp.databinding.FragmentUserBinding
+import mx.nutritivalabs.nutritivapp.homescreen.HomeScreen
 
 class UserFragment : Fragment() {
 
     private lateinit var userViewModel: UserViewModel
+    private val mAuth = FirebaseAuth.getInstance()
 
     private lateinit var binding: FragmentUserBinding
 
@@ -23,8 +28,21 @@ class UserFragment : Fragment() {
         //userViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
 
         binding = FragmentUserBinding.inflate(layoutInflater)
+        configurarEventos()
 
         return binding.root
+    }
+
+    private fun configurarEventos() {
+        binding.btnLogOut.setOnClickListener{
+            signOut()
+        }
+    }
+
+    private fun signOut() {
+        mAuth.signOut()
+        val intentMainActivity = Intent(context, MainActivity::class.java)
+        startActivity(intentMainActivity)
     }
 
     override fun onDestroyView() {
